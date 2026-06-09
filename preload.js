7df96cdc30433183f03291d0dd69f3a78f64f0c9
hhy <hhy@xhcy.com>
2026-06-09 17:05:30 +0800

    preload提交

--- preload.js
+++ preload.js
@@ -2,7 +2,9 @@ console.log("=== preload.js 开始加载 ===");
 const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('electronAPI', {
   writeFile: (fileName, content) => ipcRenderer.invoke('write-file', fileName, content),
-  openFileDialog: () => ipcRenderer.invoke('open-file-dialog'),
+  openFileDialog: (filters, properties) => ipcRenderer.invoke('open-file-dialog', filters, properties),
+  copyFile: (sourcePath, destPath) => ipcRenderer.invoke('copy-file', sourcePath, destPath),
+  copyFiles: (sourcePath, destPath) => ipcRenderer.invoke('copy-files', sourcePath, destPath),
   createFile: (fileName, jsonContent) => ipcRenderer.invoke('create-file', fileName, jsonContent),
   readFile: (filePath) => ipcRenderer.invoke('get-file', filePath),
   readFolder: (folderPath) => ipcRenderer.invoke('get-folder', folderPath),
@@ -10,4 +12,6 @@ contextBridge.exposeInMainWorld('electronAPI', {
   saveEditorMapJson: (jsonContent) => ipcRenderer.invoke('save-editor-map-json', jsonContent),
   readEditorMapJson: () => ipcRenderer.invoke('read-editor-map-json'),
   loadAreaImages: (areaName) => ipcRenderer.invoke('load-area-images', areaName),
+  excelToJson: (excelPath, jsonPath) => ipcRenderer.invoke('excelToJson', excelPath, jsonPath),
+  jsonToExcel: (jsonPath, excelPath) => ipcRenderer.invoke('jsonToExcel', jsonPath, excelPath),
 });
